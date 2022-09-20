e12c13ac1b2c73679f8f374621a432b1d0c5eddd
johnlombardi389 <lombardij38@gmail.com>
2022-09-20 16:32:11 -0400

    Progress on generating random grid

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-/*
-for (let i = 0; 1 < cards; i++) {
-  const grid_item = document.createElement("div");
-  grid_item.classList.add("card");
-
-  grid_item.setAttribute(
-    "style",
-    `
-      grid-column: span ${Math.floor(Math.random() * columnCount) + 1};
-      grid-row: span ${Math.floor(Math.random() * 3) + 1};
-    `
-  );
-
-  grid_div.appendChild(grid_item);
-}
-*/
-
+let columnSize = 3;
+let cards = 20;
 const grid_div = document.querySelector(".grid");
+
+grid_div.setAttribute(
+  "style",
+  `grid-template-columns: repeat(${columnSize}, 1fr)`
+);
+grid_div.innerHTML = "";
 
 function makeCards(number) {
   const cards = number;
@@ -23,8 +14,29 @@ function makeCards(number) {
   for (let i = 0; i < cards; i++) {
     let grid_item = document.createElement("div");
     grid_item.classList.add("card");
+
+    grid_item.setAttribute(
+      "style",
+      `
+        grid-column: span ${Math.floor(Math.random() * columnSize) + 1};
+        grid-row: span ${Math.floor(Math.random() * 3) + 1};
+      `
+    );
+
     grid_div.appendChild(grid_item);
   }
 }
 
-makeCards(10);
+makeCards(cards);
+
+/*
+const numberBtn = document.querySelectorAll("[data-number]");
+
+numberBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    let columnSize = numberBtn.innerHTML;
+    console.log(columnSize);
+    console.log("Hello");
+  });
+});
+*/
