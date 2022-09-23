65bed9ecd487b14cd4d373e7acda29acd780b7b5
johnlombardi389 <lombardij38@gmail.com>
2022-09-23 14:44:14 -0400

    Fixed function to correctly take slider value and change column size

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-let cards = 50;
-const cols_slider = document.querySelector(".slider").value;
 const grid_div = document.querySelector(".grid");
-
-grid_div.setAttribute(
-  "style",
-  `grid-template-columns: repeat(${cols_slider.value}, 1fr)`
-);
-grid_div.innerHTML = "";
-
-function makeCards(cards) {
-  for (let i = 0; i < cards; i++) {
-    const grid_item = document.createElement("div");
-    grid_item.classList.add("card");
-
-    grid_item.setAttribute(
-      "style",
-      `
-        grid-column: span ${Math.floor(Math.random() * cards) + 1};
-        grid-row: span ${Math.floor(Math.random() * 3) + 1};
-      `
-    );
-
-    grid_div.appendChild(grid_item);
-  }
-}
 
 // set amount of cards when loading page
 for (let i = 0; i < 30; i++) {
@@ -39,13 +14,6 @@ for (let i = 0; i < 30; i++) {
   );
 }
 
-const generate_btn = document.querySelector(".btn");
-
-generate_btn.addEventListener("click", (e) => {
-  makeCards(cards);
-  makeCols(cols_slider);
-});
-
 // get and display slider value
 document.getElementById("slider").oninput = function () {
   sliderValue();
@@ -56,3 +24,46 @@ function sliderValue() {
   document.getElementById("sliderValue").innerHTML = `${val} Columns`;
 }
 sliderValue();
+
+// generate button using slider value
+const generate_btn = document.querySelector(".btn");
+
+generate_btn.addEventListener("click", (e) => {
+  const colsSize = parseInt(document.getElementById("sliderValue").innerHTML);
+  makeCards(colsSize);
+  console.log(colsSize);
+});
+
+// generate cards with chosen column values
+function makeCards(cols) {
+  grid_div.innerHTML = "";
+  grid_div.classList.remove("cards");
+  grid_div.setAttribute("style", `grid-template-columns: repeat(${cols}, 1fr)`);
+  for (let i = 0; i < cols; i++) {
+    const grid_item = document.createElement("div");
+    grid_item.classList.add("card");
+
+    grid_item.setAttribute(
+      "style",
+      `
+        grid-column: span ${Math.floor(Math.random() * cols) + 1};
+        grid-row: span ${Math.floor(Math.random() * 3) + 1};
+      `
+    );
+
+    grid_div.appendChild(grid_item);
+  }
+
+  for (let i = 0; i < 30; i++) {
+    const grid_item = document.createElement("div");
+    grid_item.classList.add("card");
+    grid_div.appendChild(grid_item);
+    grid_item.setAttribute(
+      "style",
+      `
+        grid-column: span ${Math.floor(Math.random() * 3) + 1};
+        grid-row: span ${Math.floor(Math.random() * 3) + 1};
+      `
+    );
+  }
+}
